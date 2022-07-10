1ab8e985bec51538f209227eb30bc8d021317284
gnoul_ <ttluong4869@gmail.com>
2022-07-11 01:06:20 +0700

    Import leaflet css

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 
+import 'leaflet/dist/leaflet.css';
 import '@/assets/styles/main.scss';
 
 const app = createApp(App);
